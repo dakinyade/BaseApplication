aacd213b8c77029129d2fc1912861e5d06171c8c
Michael Akinyade <michaelintellect2007@gmail.com>
2020-12-16 15:52:52 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,4 +2,13 @@ def buildApp(){
    echo 'building the application....'
 }
 
+
+def testApp(){
+   echo 'building the application....'
+}
+
+
+def deployApp(){
+   echo 'building the application....'
+}
 return this
